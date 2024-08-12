51af0a2114de3a30eb84736d8fb086af105920ee
Biraj <birajparajuli2001@gmail.com>
2024-08-12 10:52:22 +0545

    user profile review edit

--- components/user-profile-review.tsx
+++ components/user-profile-review.tsx
@@ -1,31 +1,31 @@
 "use client";
 import StarRatingValue from "@/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/star-rating-value";
 import TimeStampForm from "@/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/time-stamp-form";
-import { useUser } from "@clerk/nextjs";
-import Image from "next/image";
+import { useSession } from "next-auth/react";
 import { Preview } from "./preview";
 
 interface UserProfileReviewProps {
-	review: string;
-	reviewValue: number;
-	createdAt: Date;
+  review: string;
+  reviewValue: number;
+  createdAt: Date;
 }
 const UserProfileReview = ({
-	review,
-	reviewValue,
-	createdAt,
+  review,
+  reviewValue,
+  createdAt,
 }: UserProfileReviewProps) => {
-	const { isLoaded, isSignedIn, user } = useUser();
+  const session = useSession();
+  const user = session?.data?.user || null;
+  console.log("user", user);
 
-	// if (!isLoaded || !isSignedIn) {
-	// 	return null;
-	// }
-	const firstName = user?.firstName || "Challelign";
-	const lastName = user?.lastName || "Tilahun";
-	const initials = `${firstName.charAt(0)}${lastName.charAt(0)}`;
-	return (
-		<>
-			{/* <Image
+  // if (!isLoaded || !isSignedIn) {
+  // 	return null;
+  // }
+  const name = user?.name || "Jhon Timalsina";
+  const initials = `${name.charAt(0, 1)}`;
+  return (
+    <>
+      {/* <Image
 				width={100}
 				height={100}
 				src={
@@ -37,28 +37,28 @@ const UserProfileReview = ({
 				alt="user profile"
 			/> */}
 
-			<div className="w-12 h-12 rounded-full bg-gray-500 text-white flex items-center justify-center">
-				<span className="text-lg font-bold">{initials}</span>
-			</div>
-			<div>
-				<p className="text-[15px] text-[#333] font-bold">
-					{user?.firstName ? user?.firstName : "Unknown user"}
-				</p>
-				<p className="flex items-center gap-x-4 text-xs text-gray-500 mt-0.5">
-					<StarRatingValue starSize="text-xl" value={reviewValue!!} />
-					<TimeStampForm createdAt={createdAt} />
-				</p>
-			</div>
+      <div className="w-12 h-12 rounded-full bg-gray-500 text-white flex items-center justify-center">
+        <span className="text-lg font-bold">{initials}</span>
+      </div>
+      <div>
+        <p className="text-[15px] text-[#333] font-bold">
+          {user?.name ? user?.name : "Unknown user"}
+        </p>
+        <p className="flex items-center gap-x-4 text-xs text-gray-500 mt-0.5">
+          <StarRatingValue starSize="text-xl" value={reviewValue!!} />
+          <TimeStampForm createdAt={createdAt} />
+        </p>
+      </div>
 
-			<div className="bg-slate-100 rounded-md">
-				<div className="flex justify-between items-center">
-					<div>
-						<Preview value={review!} />
-					</div>
-				</div>
-			</div>
-		</>
-	);
+      <div className="bg-slate-100 rounded-md">
+        <div className="flex justify-between items-center">
+          <div>
+            <Preview value={review!} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserProfileReview;
